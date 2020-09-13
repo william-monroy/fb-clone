175c972f9d26f66b1c3054e6e1ac2dc760c78715
William Monroy <william_monroy777@hotmail.com>
2020-09-13 12:53:28 -0500

    Primer avance - Header

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCEB_tu4T19tWemUAM3JOy7LatwsSoDfJY",
+    authDomain: "facebook-clone-227e6.firebaseapp.com",
+    databaseURL: "https://facebook-clone-227e6.firebaseio.com",
+    projectId: "facebook-clone-227e6",
+    storageBucket: "facebook-clone-227e6.appspot.com",
+    messagingSenderId: "946115794670",
+    appId: "1:946115794670:web:cd17c34ca9fb3c8390c5ff",
+    measurementId: "G-BTVS56W9TV"
+};
