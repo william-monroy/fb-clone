f8ed20eb96f3244e2bcbee4cc5d66124cc9a88c5
William Monroy <william_monroy777@hotmail.com>
2020-09-13 22:42:39 -0500

    MessageSender en Feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./Feed.css";
+import MessageSender from './MessageSender';
 import StoryReel from "./StoryReel"
 
 function Feed() {
     return <div className="feed">
         <StoryReel />
-        { /* StoryReel */}        
-        { /* MessageSender */}
+        <MessageSender />
     </div>
 }
 
