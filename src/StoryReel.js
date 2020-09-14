0fb33ed7c9ef0ed09a65dd7cebc19474bf5b2445
William Monroy <william_monroy777@hotmail.com>
2020-09-13 21:08:49 -0500

    StoryReel y estilos para Sidebar

---
+++ src/StoryReel.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Story from "./Story";
+import './StoryReel.css';
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https://avatars0.githubusercontent.com/u/25649623?s=460&u=78347d552336985bb1189fa38324fa2ec17d0ea3&v=4"
+                title="Sofia Vega"
+            />
+            <Story
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBHC2s4NFdzXEsVzvBPGjkrSePQa-8XFuNtQ&usqp=CAU"
+                profileSrc="https://avatars0.githubusercontent.com/u/52941810?s=460&u=3e1c4fb401b7ba9929e3c536bba6ef08a85fe957&v=4"
+                title="Luis Eduardo Gutiérrez Saenz Pardo"
+            />
+            <Story
+                image="https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                profileSrc="https://avatars2.githubusercontent.com/u/67978673?s=460&u=1683ea741837e6c9232e08fca033d607cc19ac57&v=4"
+                title="RJOlmedo"
+            />
+            <Story
+                image="https://webcdn-adespressoinc.netdna-ssl.com/wp-content/uploads/2019/11/amazing-example-fb-ig-stories-that-convert-01.jpg"
+                profileSrc="https://avatars1.githubusercontent.com/u/18475586?s=460&u=e9f554f67c5aa90ac9afe423023fed68607091ea&v=4"
+                title="Jiaju Zhuang"
+            />
+            <Story
+                image="https://scontent.faqp2-1.fna.fbcdn.net/v/t1.0-9/50343401_2085520224871831_5559277063736655872_n.png?_nc_cat=106&_nc_sid=730e14&_nc_eui2=AeFsYGKJPKvArCupwgBhhGNOYBfICW41xWpgF8gJbjXFarwLkyX0YHHzO2e8APwXwms9Oq1F4fqaZvcYB80-36WP&_nc_ohc=OTR_QRnl6O4AX-ouE3U&_nc_ht=scontent.faqp2-1.fna&oh=d2b0b4ead9b0574161fc937917c6ba95&oe=5F86209F"
+                profileSrc="https://avatars2.githubusercontent.com/u/22975773?s=460&u=1dc7dd93dcab1c8fc90ecc34abda7eb276422118&v=4"
+                title="jipengfei-jpf"
+            />
+        </div>
+    )
+}
+
+export default StoryReel
