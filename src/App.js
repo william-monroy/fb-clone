6cf9daf8560277a14130f02b1d5afe3f0ff00e15
William Monroy <william_monroy777@hotmail.com>
2020-09-14 00:35:50 -0500

    Login design

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widgets from './Widgets';
+import Login from './Login';
 
 function App() {
+    const user = null;
     return (
         //BEM Naming convention
         <div className = "app" >
-            <Header />
-            <div className="app__body">
-                <Sidebar />
-                <Feed />
-                <Widgets />
-            </div>
+            {!user ? 
+                <Login /> 
+            : (
+                <>
+                <Header />
+
+                <div className="app__body">
+                    <Sidebar />
+                    <Feed />
+                    <Widgets />
+                </div>
+                </>
+            )}
+            
             
         </div>
     );
---
+++ src/Login.js
@@ -0,0 +1,25 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+import './Login.css'
+
+function Login() {
+
+    const signIn = () => {
+        //Sign In...
+    }
+
+    return (
+        <div className="login">
+            <div className="login__logo">
+                <img 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/800px-Facebook_f_logo_%282019%29.svg.png" alt="" />
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Facebook_Logo_%282019%29.svg/1920px-Facebook_Logo_%282019%29.svg.png" className="letter" alt="" />
+            </div>
+            <Button type="submit" onClick={signIn}>Sign In
+            </Button>
+        </div>
+    )
+}
+
+export default Login
